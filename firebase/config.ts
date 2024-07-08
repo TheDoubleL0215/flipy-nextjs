d8803af5b07c6f99d4703400be15c4bab87ad243
TheDoubleL0215 <levai.levente.gabor@gmail.com>
2024-07-08 19:03:20 +0200

    refactor: firebase config variables are hardcoded

--- firebase/config.ts
+++ firebase/config.ts
@@ -4,13 +4,14 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+    apiKey: "AIzaSyCFz9wKr98Jox3BjP3DEl1rxRgieySKIOo",
+    authDomain: "flipy-flashcards.firebaseapp.com",
+    projectId: "flipy-flashcards",
+    storageBucket: "flipy-flashcards.appspot.com",
+    messagingSenderId: "1248505269",
+    appId: "1:1248505269:web:2e53506b95fc222d40bdfc"
 };
+
 
 // Initialize Firebase
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
